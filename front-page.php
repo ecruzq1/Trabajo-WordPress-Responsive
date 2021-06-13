020b3441f00e72d03a7df9cfa2c7d81c06537e2b
ecruzq1 <e.p.c.q.94@gmail.com>
2021-06-12 22:58:15 -0500

    Crear pagina usando tailwind css responsive Edwin Cruz

---
+++ front-page.php
@@ -0,0 +1,137 @@
+<?php get_header()?>
+
+<main class=" bg-blue-200 md:bg-gray-200 ... font-sans md:font-mono"> 
+<div class="p-8 mt-2 flex items-center space-x-10  sm:text-left">
+<div class="w-1/12">
+<img 
+class="rounded-full"
+src="<?php echo get_template_directory_uri() ?>/assets/img/edwin.jpg" 
+alt="imagen perfil">
+
+</div>
+
+<div class="w-6/12">
+<br>
+<p>Mi Nombre es Edwin Cruz actualmente me encuentro cursando ultimo semestre de Analisis en Sistemas tengo 24 años mi meta principal es seguir preparandome en los distintos lenguajes de programacion existentes ya que cada uno de ellos nos ofrecen diferentes funcionalidades 
+en mis tiempos libres me gusta hacer deporte entre mis favoritos son: futbol,basket,volley 
+
+
+</div>
+<div class="w-1/12">
+<img 
+class="rounded-full"
+src="<?php echo get_template_directory_uri() ?>/assets/img/futbol.jpg" 
+alt="imagen perfil">
+
+</div>
+<div class="w-1/12">
+<img 
+class="rounded-full"
+src="<?php echo get_template_directory_uri() ?>/assets/img/volley.jpg" 
+alt="imagen perfil">
+
+</div>
+<div class="w-1/12">
+<img 
+class="rounded-full"
+src="<?php echo get_template_directory_uri() ?>/assets/img/basketball.jpeg" 
+alt="imagen perfil">
+
+
+</div>
+
+
+</div>
+<div class="p-8 mt-2 flex items-center space-x-10">
+Estudios Realizados : <br>
+
+<div class="w-6/12">
+<br>
+<p>• Primaria   : Unidad Educativa San Jose La Salle 
+<p>• Secundaria : Unidad Educativa San Jose La Salle
+<p>• Superior   : Instituto Tecnologico Quito
+
+
+
+</div>
+
+</div>
+<div class="p-8 mt-2 flex items-center space-x-10">
+Cursos Realizados : <br>
+
+<div class="w-6/12">
+<br>
+<p>• Gestion Empresarial  : Mantenimiento Computadoras 
+<p>• Instituto Guayasamin : Ingles Intermedio
+
+</div>
+</div>
+<div class="p-8 mt-2 flex items-center space-x-10">
+Tecnologias Aprendidas<br>
+
+<div class="w-1/12">
+<img 
+class="rounded-full"
+src="<?php echo get_template_directory_uri() ?>/assets/img/Java-Logo.jpg" 
+alt="imagen perfil">
+
+</div>
+
+<div class="w-1/12">
+<img 
+class="rounded-full"
+src="<?php echo get_template_directory_uri() ?>/assets/img/csharp.jpg" 
+alt="imagen perfil">
+</div>
+
+<div class="w-1/12">
+<img 
+class="rounded-full"
+src="<?php echo get_template_directory_uri() ?>/assets/img/php.jpg" 
+alt="imagen perfil">
+
+</div>
+
+<div class="w-1/12">
+<img 
+class="rounded-full"
+src="<?php echo get_template_directory_uri() ?>/assets/img/sql.jpg" 
+alt="imagen perfil">
+
+</div>
+
+<div class="w-1/12">
+<img 
+class="rounded-full"
+src="<?php echo get_template_directory_uri() ?>/assets/img/my.png" 
+alt="imagen perfil">
+</div>
+
+<div class="w-1/12">
+<img 
+class="rounded-full"
+src="<?php echo get_template_directory_uri() ?>/assets/img/postgresql.jpg" 
+alt="imagen perfil">
+
+</div>
+<div class="w-1/12">
+<img 
+class="rounded-full"
+src="<?php echo get_template_directory_uri() ?>/assets/img/pseint.png" 
+alt="imagen perfil">
+
+</div>
+
+<div class="w-1/12">
+<img 
+class="rounded-full"
+src="<?php echo get_template_directory_uri() ?>/assets/img/cisco.jpg" 
+alt="imagen perfil">
+
+</div>
+ </main>
+
+<?php get_footer()?>
+
+
+
